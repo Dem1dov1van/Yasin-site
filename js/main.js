ae7e6e8df33a7a714bbf6daacef6593b4a495ece
elik <elik@example.com>
2021-05-16 21:50:38 +0300

    finalMaybe

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// slider
+$(document).ready(function(){
+   $('.slider').slick({
+      dots: true,
+      autoplay: true,
+   })
+})
+
+// paralax
+
+$('.history').on('mousemove', (e)=>{
+   const x = e.pageX / $(window).width();
+   const y = e.pageY / $(window).height();
+
+   $('.wrap-body__graphic').css('transform', 
+   'translate(' + x * 5 + 'px, ' + y * 5 + 'px)'
+   );
+});
+
+$('.history').on('mousemove', (e)=>{
+   const x = e.pageX / $(window).width();
+   const y = e.pageY / $(window).height();
+
+   $('.parralax__circle--dad').css('transform', 
+   'translate(-' + x * 7 + 'px,-' + y * 7 + 'px)'
+   );
+});
+
+
+
+
+
